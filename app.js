d895e959b8abd079e6f81bd82dd904873627e250
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-11 21:51:22 +0600

    https get request

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const  express = require("express");
+const express = require("express");
+const https = require("https");
 
 const app = express();
 
 app.get("/", function(require, respond){
+
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9367517c4a3d0682ce29a352cfe1cf6a";
+
+    https.get(url,function(response){
+        console.log(response);
+    })
+
     respond.send("Server is up and running");
 })
 
