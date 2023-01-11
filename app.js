fb3b88e168cdc7e8a7865bd989a37459e7c45cdb
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-01-11 22:17:51 +0600

    JSON parsed

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ app.get("/", function(require, respond){
     var url = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9367517c4a3d0682ce29a352cfe1cf6a";
 
     https.get(url,function(response){
-        console.log(response);
+        console.log(response.statusCode);
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.weather[0].description;
+
+            console.log(temp);
+        })
+
     })
 
     respond.send("Server is up and running");
